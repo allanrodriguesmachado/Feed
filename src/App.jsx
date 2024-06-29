9153052512c529827c9fbde1858d5a11a193c0e7
unknown <allan.rodrigues14@hotmail.com>
2024-06-28 23:10:14 -0300

    Creating Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Header } from './components/Header'
-
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export default function App() {
   return (
-    <Header/>
+    <>  
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>POST</main>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css'
+import headerSider from '../assets/a.jpg'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+           <img className={styles.cover} src="https://images.unsplash.com/photo-1584947113929-2d02f9c06203?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzAwfHxEZXZlbG9wZXJ8ZW58MHx8MHx8fDI%3D" alt="Logo Sidebar" />
+        
+            <div className={styles.profile}>
+                <strong>Allan Rodrigues</strong>
+                <span>Full Stack Developer</span>
+            </div>
+
+            <footer className={styles.footer}>
+                <a href="#">Editar Perfil</a>
+            </footer>
+        </aside>
+        
+    )
+}
