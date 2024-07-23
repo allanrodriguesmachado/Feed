186037517c25cfdff4ff47c2ec221ae2c7cc133a
unknown <allan.rodrigues14@hotmail.com>
2024-07-22 21:54:46 -0300

    Like Message

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,8 +4,14 @@ import {Avatar} from "./Avatar.jsx";
 import { useState } from 'react';
 
 export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
     }
 
     return (
@@ -28,9 +34,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
